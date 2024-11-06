0777ec676035fee74bf08a9e58b225db498b68fd
Ziyao Ma <yumeng_xu@outlook.com>
2024-11-06 13:42:33 +0800

    add evaluation part

--- tag_recommender.py
+++ tag_recommender.py
@@ -6,11 +6,11 @@ from typing import List, Dict
 
 forward_index_data = {}
 example_index_data = {}
-
+evaluation_data = {}
 
 def load_data():
     # load and store data into dicts
-    global forward_index_data, example_index_data
+    global forward_index_data, example_index_data, evaluation_data
     
     with open('forward_indexes_full_dataset.jsonl', 'r') as f:
         for line in f:
@@ -21,6 +21,9 @@ def load_data():
         for line in f:
             entry = json.loads(line.strip())
             example_index_data.update(entry)
+    with open('evaluation_data.json', 'r', encoding='utf-8') as f:
+            entry = json.load(f)
+            evaluation_data.update(entry)
 
 def calculate_score(fields: Dict[str, float], query_tag_frequency) -> float:
     #calculate ranking score
@@ -33,7 +36,7 @@ def calculate_score(fields: Dict[str, float], query_tag_frequency) -> float:
     score_dict["cos_similarity"] = cos_similarity
     tf_idf = fields.get("tf_idf", 0)
     score_dict["tf_idf"] = tf_idf
-    final_score = (0.5 * cos_similarity + 2 * jaccard_similarity) * 0.003 * math.log(tf_idf)
+    final_score = 0.1 * cos_similarity + 15 * jaccard_similarity + 0.05 * math.log(tf_idf)
     score_dict["final_score"] = final_score
     return score_dict
 
@@ -67,6 +70,7 @@ def suggest_tags(query: str, limit: int = 5) -> List[Dict[str, float]]:
         
         # Prepare final output
         output = []
+        output_tag_list = []
         for result in sorted_results:
             output.append({
                 result["tag"]: {
@@ -76,18 +80,44 @@ def suggest_tags(query: str, limit: int = 5) -> List[Dict[str, float]]:
                     "tf_idf": result["tf_idf"]
                 }
             })
-        print(output)
-        return output
+            output_tag_list.append(result["tag"])
+        #print(output)
+        return {query:output_tag_list}
     except Exception as e:
         print(f"Error occurred: {e}, query may not exist in the indexes")
 
+def evaluation(evaluation_data, result_data):
+    #evaluation part: hit_ratio, recall and precision
+    hits = 0
+    recall = 0
+    precision = 0
+    total_len = 0
+    for key in evaluation_data:
+        if result_data[key]:
+            hits += any(tag in evaluation_data[key] for tag in result_data[key])
+            recall += len(set(result_data[key]).intersection(evaluation_data[key]))/len(evaluation_data[key]) if evaluation_data[key] else 0
+            precision += len(set(result_data[key]).intersection(evaluation_data[key])) / len(result_data[key]) if result_data[key] else 0
+            total_len += 1
+            if total_len < 20:
+                print("query: ", key)
+                print("evaluation_data: ",evaluation_data[key])
+                print("result_data: ",result_data[key])
+        else:
+            #print(key)
+            continue
+    print("hit_ratio: ", hits/total_len)
+    print("recall: ", recall/total_len)
+    print("precision: ", precision/total_len)
 
 if __name__ == "__main__":
     load_data()
-    query = "funny"
-    limit = 10
-    start_time = time.time()  
-    suggest_tags(query, limit) 
-    end_time = time.time()  
-    latency = end_time - start_time  
-    print(f"Latency: {latency:.4f} seconds")
+    result_data = {}
+    for key in evaluation_data:
+        #query = "funny"
+        limit = 20
+    #start_time = time.time() 
+        result_data.update(suggest_tags(key, limit))
+    evaluation(evaluation_data, result_data)
+    #end_time = time.time()  
+    #latency = end_time - start_time  
+    #print(f"Latency: {latency:.4f} seconds")
